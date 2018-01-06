wummel/linkchecker

fixes for Python 3: fix running problems in Python 3

linkcheck/strformat.py
@@ -59,7 +59,7 @@ def unicode_safe (s, encoding=i18n.default_encoding, errors='replace'):
     if isinstance(s, str_text):
         # s is already unicode, nothing to do
         return s
-    return str_text(s)
+    return str_text(str(s), encoding, errors)
 
 
 def ascii_safe (s):

PetrDlouhy/linkchecker-1

linkcheck/strformat.py
@@ -59,7 +59,7 @@ def unicode_safe (s, encoding=i18n.default_encoding, errors='replace'):
     if isinstance(s, text):
         # s is already unicode, nothing to do
         return s
-    return text(bytes(s), encoding, errors)
+    return text(str(s), encoding, errors)
 
 
 def ascii_safe (s):